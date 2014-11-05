cf423566ed4eca091b38792b50954bcacb9f9c7a
Jason Thomas <jthomas@silvervue.com>
2014-11-04 19:09:00 -0700

    Error in script checking platform version for 14.04

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,7 +13,7 @@ when "amazon"
 when "debian", "ubuntu"
   default["s3fs"]["packages"] = %w{build-essential pkg-config libcurl4-openssl-dev libfuse-dev fuse-utils libfuse2 libxml2-dev mime-support}
   default["fuse"]["version"] = "2.8.7"
-  case node["platform_version"].to_i
+  case node["platform_version"].to_f
   when 14.04
     default["s3fs"]["packages"] = %w{build-essential pkg-config libcurl4-openssl-dev libfuse-dev fuse-emulator-utils libfuse2 libxml2-dev mime-support}
   end
